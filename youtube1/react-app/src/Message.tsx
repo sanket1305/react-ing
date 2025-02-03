a0896c7d1c670dcfce8222dd1c3cf04c335176e0
sanket1305 <meetsanket24@gmail.com>
2025-02-03 16:01:32 -0500

    Added some react notes and tried out classic Hello World

---
+++ youtube1/react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing.. required in react
+function Message() {
+    // JSX: JavaScript xml
+    const name = '';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World!!!</h1>
+
+}
+
+export default Message;
