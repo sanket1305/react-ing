e223facfd43ce813416499674349fbb069c01acd
sanket1305 <meetsanket24@gmail.com>
2025-02-03 16:57:18 -0500

    conditional rendering

--- youtube1/react-app/src/components/ListGroup.tsx
+++ youtube1/react-app/src/components/ListGroup.tsx
@@ -1,12 +1,22 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // functions are useful if we want to pass parameters
+  // if you don't have any parameters, then it's convinient to use variables like above 'message'
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  }
 
   return (
     // we can also use <> </> to use fragments without actually importing them
     <Fragment>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
