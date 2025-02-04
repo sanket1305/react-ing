0791421d909fa855018ebd423f5618506616f43f
sanket1305 <meetsanket24@gmail.com>
2025-02-04 18:24:37 -0500

    handling events using function

--- youtube1/react-app/src/components/ListGroup.tsx
+++ youtube1/react-app/src/components/ListGroup.tsx
@@ -1,8 +1,10 @@
-import { Fragment } from "react";
+import { Fragment, MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   const message = items.length === 0 ? <p>No item found</p> : null;
 
@@ -10,7 +12,7 @@ function ListGroup() {
   // if you don't have any parameters, then it's convinient to use variables like above 'message'
   const getMessage = () => {
     return items.length === 0 ? <p>No item found</p> : null;
-  }
+  };
 
   return (
     // we can also use <> </> to use fragments without actually importing them
@@ -18,8 +20,14 @@ function ListGroup() {
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
